53a4049c3dfa2f3d9f782d51f8490b2efe46cfaa
Raymond Pressly <raynicho@umich.edu>
2024-04-01 19:28:23 -0400

    Add jooq

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
+import org.jooq.meta.jaxb.Jdbc
 
 plugins {
     id("org.springframework.boot") version "3.2.4"
@@ -7,6 +10,7 @@ plugins {
     id("org.liquibase.gradle") version "2.2.0" apply true
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("io.gitlab.arturbosch.detekt") version "1.23.6" apply true
+    id("org.jooq.jooq-codegen-gradle") version "3.19.6"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
 }
@@ -30,6 +34,14 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api")
     implementation("org.postgresql:postgresql")
     implementation("org.liquibase:liquibase-core")
+    implementation("org.jooq:jooq-codegen:3.19.6")
+    implementation("org.jooq:jooq-kotlin:3.19.6")
+    implementation("org.jooq:jooq:3.19.6")
+
+    jooqCodegen("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
+    jooqCodegen("org.jooq:jooq-meta-extensions:3.19.6")
+    jooqCodegen("org.jooq:jooq-meta-kotlin:3.19.6")
+    jooqCodegen("org.postgresql:postgresql")
 
     liquibaseRuntime("org.liquibase:liquibase-core")
     liquibaseRuntime("org.postgresql:postgresql")
@@ -38,6 +50,7 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
+    dependsOn("jooqCodegenMain")
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
@@ -46,6 +59,16 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<KtLintFormatTask> {
+    dependsOn("jooqCodegenMain")
+    exclude("**/generated/**")
+}
+
+tasks.withType<KtLintCheckTask> {
+    dependsOn("jooqCodegenMain")
+    exclude("**/generated/**")
 }
 
 detekt {
@@ -62,4 +85,42 @@ tasks.withType<Detekt>().configureEach {
         md.required = true
     }
     basePath = rootDir.absolutePath
+    dependsOn("jooqCodegenMain")
+    exclude("**/generated/**")
+}
+
+jooq {
+    version = "3.19.6"
+    configuration { }
+
+    executions {
+        create("main") {
+            configuration {
+                logging = org.jooq.meta.jaxb.Logging.DEBUG
+                jdbc = Jdbc()
+
+                jdbc.url = System.getenv("PSQL_URL")
+                jdbc.user = System.getenv("PSQL_USER")
+                jdbc.password = System.getenv("PSQL_PASSWORD")
+
+                generator {
+                    name = "org.jooq.codegen.KotlinGenerator"
+
+                    database {
+                        inputSchema = "public"
+                    }
+
+                    generate {
+                        isPojosAsKotlinDataClasses = true // use data classes
+                        isImplicitJoinPathsToMany = false
+                    }
+
+                    target {
+                        packageName = "tech.pic.portal.jvm.generated"
+                        directory = "$projectDir/src/main/kotlin/"
+                    }
+                }
+            }
+        }
+    }
 }
